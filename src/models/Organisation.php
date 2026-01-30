c36b89396138e279e9b46f58696528810e7273aa
Josh Crawford <joshua@sgroup.com.au>
2026-01-30 22:45:41 +1100

    Update line item tax default value to be “Inclusive”

--- src/models/Organisation.php
+++ src/models/Organisation.php
@@ -49,7 +49,7 @@ class Organisation extends SavableComponent implements OAuthProviderInterface, S
     public ?string $accountRounding = null;
     public ?string $accountDiscounts = null;
     public ?string $accountAdditionalFees = null;
-    public ?string $accountLineItemTax = 'Exclusive';
+    public ?string $accountLineItemTax = 'Inclusive';
     public ?string $accountInvoiceStatus = 'AUTHORISED';
     public ?int $sortOrder = null;
     public ?string $uid = null;
